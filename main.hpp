7ebc061f040388342e57907d25cb4a8c08a9b730
Rollin Gilman <rdgilman@my.canyons.edu>
2026-05-10 15:52:19 -0700

    a73 Circular Left Shift in Array

--- main.hpp
+++ main.hpp
@@ -1,6 +1,6 @@
 // Circular N left rotation in Arrays
 // [1,2,3,4,5] and we want to make 3 left rotation => Result is [4,5,1,2,3]
-// [10,13,5,7,2] and we want to make 1 left rotation => Result is [13,5,7,2,10]
+// [10,13,5,7,2] and we want to make 1-left rotation => Result is [13,5,7,2,10]
 // Do not use the additional array. In-place swapping.
 
 #include <iostream>
@@ -12,11 +12,25 @@ void printout(int[], int);
 
 void shiftleft(int number[], int SIZE, int count)
 {
+    count = count % SIZE;
+
+    for (int c = 0; c < count; c++)
+    {
+        int first = number[0];
+
+        for (int i = 0; i < SIZE - 1; i++)
+        {
+            number[i] = number[i + 1];
+        }
+
+        number[SIZE - 1] = first;
+    }
 }
+
 void printout(int number[], int SIZE)
 {
-	for (int i = 0; i < SIZE; i++)
-		cout << setw(5) << number[i];
-	cout << endl;
-	return;
+    for (int i = 0; i < SIZE; i++)
+        cout << setw(5) << number[i];
+
+    cout << endl;
 }
